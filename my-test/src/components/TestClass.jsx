59fd4cfc27cdb20b17cfc93d8534f1e0d269e3c4
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-17 21:26:32 +0500

    new css file

--- my-test/src/components/TestClass.jsx
+++ my-test/src/components/TestClass.jsx
@@ -6,19 +6,3 @@ export default function TestClass() {
   )
 }
 
-/* 
-echo "# reactLesson" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:javohirbekkhaydarov/reactLesson.git
-git push -u origin main */
-
-/*
-
-git remote add origin git@github.com:javohirbekkhaydarov/reactLesson.git
-git branch -M main
-git push -u origin main
-
-*/
