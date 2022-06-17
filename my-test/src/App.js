9a28b27c1394ebaa597b9dbdd4e6f3fa7fa4708d
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-17 21:10:32 +0500

    first commit

---
+++ my-test/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import TestClass from "./components/TestClass";
+function App() {
+  return (
+    <div className="App">
+      <TestClass />
+    </div>
+  );
+}
+
+export default App;
