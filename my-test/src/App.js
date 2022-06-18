c8bc3ec0dfabd7938481304893f5b67cace7de65
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-18 05:42:15 +0500

    create class component

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import TestClass from "./components/TestClass";
+import Test2 from "./components/Test2";
 function App() {
   return (
     <div className="App">
       <TestClass />
+      <Test2 />
     </div>
   );
 }
---
+++ my-test/src/components/Test2.jsx
@@ -0,0 +1,7 @@
+function test2() {
+  const expeneDate = new Date(2022, 6, 18);
+  const expenseTitle = "The coursen for programming";
+  const expenseAmount = 500000;
+  return <div>Date: {expeneDate.toString()}</div>
+}
+export default test2
