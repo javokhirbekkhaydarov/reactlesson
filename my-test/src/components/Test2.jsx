9fdecea8c465f7e5adc502a885eb2c5268187ff5
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-18 05:58:05 +0500

    added props

--- my-test/src/components/Test2.jsx
+++ my-test/src/components/Test2.jsx
@@ -2,6 +2,12 @@ function test2() {
   const expeneDate = new Date(2022, 6, 18);
   const expenseTitle = "The coursen for programming";
   const expenseAmount = 500000;
-  return <div>Date: {expeneDate.toString()}</div>
+  return (
+    <div>
+      Date: {expeneDate.toString()}
+      <h2>Why: {expenseTitle}</h2>
+      <p>Price {expenseAmount}</p>
+    </div>
+  );
 }
-export default test2
+export default test2;
