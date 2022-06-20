77eb16604a546367b93e92725b49313f10ec2560
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-21 02:04:14 +0500

    thin working

--- my-test/src/components/Book.jsx
+++ my-test/src/components/Book.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import "./Book.css";
 const Book = (props) => {
-
+  const [title, setTitle] = useState(props.title);
+  let title = props.title;
   const clickHunter = (props) => {
     console.log("clicked");
-  }
+  };
 
   return (
     <div className="book">
       <img className="book_img" src={props.img} alt="deep work" />
       <div className="book_desc">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <p>{props.author}</p>
         <button onClick={() => console.log(`clicked`)}> Change title</button>
       </div>
