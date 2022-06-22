a939b0bb6ae687306a509d801836ebaa822624b6
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-22 05:31:05 +0500

    added increase and decrease number

--- my-test/src/components/Counter.jsx
+++ my-test/src/components/Counter.jsx
@@ -6,10 +6,15 @@ const Counter = () => {
   const addNumberHandler = () => {
     setCounter(counter + 1);
   };
+  const removeNumberHandler = () => {
+    setCounter(counter - 1);
+  };
   return (
     <div>
       <p>{counter}</p>
       <button onClick={addNumberHandler}>Add</button>
+      <button onClick={removeNumberHandler}>Remove</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
     </div>
   );
 };
