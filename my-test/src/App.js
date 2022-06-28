06755a983c4f2fbea2558fbd98b053a5ecab5dcd
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-28 17:00:35 +0500

    added new fiels

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import MultipleRanding from "./components/canditionalRender/MultipleRanding";
 import UseEffectFetchData from "./components/useEffect/UseEffectFetchData";
 
 function App() {
  
   return (
     <div className="container">
-     <UseEffectFetchData />
+     <MultipleRanding />
     </div>
   );
 }
---
+++ my-test/src/components/canditionalRender/MultipleRanding.jsx
@@ -0,0 +1,15 @@
+import React , {useState , useEffect} from 'react'
+
+
+const url = "https://api.github.com/users/QuincyLarson"
+const MultipleRanding = () => {
+  return (
+    <>
+    <div className="container">
+        <h1>hi everyone</h1>
+    </div>
+    </>
+  )
+}
+
+export default MultipleRanding
