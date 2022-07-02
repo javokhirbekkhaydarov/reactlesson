d9d4b00b766d2b51041a5f075bb3195c8693f069
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-02 06:18:05 +0500

    added userudecer components and modal

---
+++ my-test/src/components/UseReducer/UseReducer.jsx
@@ -0,0 +1,9 @@
+import React, {useState , useReducer} from 'react'
+import Modal from './Modal'
+const UseReducer = () => {
+  return (
+    <div>UseReducer</div>
+  )
+}
+
+export default UseReducer
