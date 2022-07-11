5eec36b3fb078dfa611c5802dbd6b9bd514ab1ae
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-12 00:06:21 +0500

    give styles and js codes

--- my-test/src/components/reactProjectLesson19/Main.js
+++ my-test/src/components/reactProjectLesson19/Main.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Main.css";
 
+const url = "https://course-api.com/react-tours-project";
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [loading, SetLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
-export default Main
+  const fetchTours = async () => {
+    SetLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      SetLoading(false);
+      setTours(tours);
+    } catch (error) {
+      SetLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  return <div>Main</div>;
+};
+
+export default Main;
