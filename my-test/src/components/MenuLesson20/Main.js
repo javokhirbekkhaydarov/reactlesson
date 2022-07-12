25f46b9d2854faf317b3403f267577c9da4d51be
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-12 23:38:24 +0500

    added images and items

--- my-test/src/components/MenuLesson20/Main.js
+++ my-test/src/components/MenuLesson20/Main.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import items from "./data";
 import "./Main.css";
+import Categories from "./Categories";
 
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 const Main = () => {
@@ -9,14 +10,29 @@ const Main = () => {
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if(category === "all") {
-        setMenuItems(items);
-        return;
+    if (category === "all") {
+      setMenuItems(items);
+      return;
     }
 
-    const newItems = items.filter
-  }
-  return <div>Main</div>;
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>
+            Our Menu
+            <div className="underline"></div>
+          </h2>
+        </div>
+
+        <Categories categories={categories} filterItem={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 
 export default Main;
