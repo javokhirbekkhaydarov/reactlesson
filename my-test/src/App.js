add1cebad1a774090f7baed75a3e5b9de360b425
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-12 22:45:52 +0500

    added lesson20 and components

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 
-import Main from "./components/reactProjectLesson19/Main";
+import Main from "./components/MenuLesson20/Main";
 function App() {
   return (
     <div className="container">
       <Main />
     </div>
   );
-
 }
 
 export default App;
