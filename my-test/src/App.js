beb42d699d613e94ed8aa9212edddc6f0765314b
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-18 23:53:32 +0500

    added ne branch and new components Cocktails

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Main from "./components/Lesson21Store/Main";
+import Hero from "./components/Lesson22Coctalils/Hero";
 function App() {
   return (
     <div className="container">
-      <Main />
-    </div>
+      <Hero />
+      
+          </div>
   );
 }
 
