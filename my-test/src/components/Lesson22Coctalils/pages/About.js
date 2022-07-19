a0d8f74f2be1e75e65a5006541a077009a8ab065
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-19 20:54:21 +0500

    added App provider and context api

--- my-test/src/components/Lesson22Coctalils/pages/About.js
+++ my-test/src/components/Lesson22Coctalils/pages/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title"> about us</h1>
 
-export default About
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, minima!
+        Error perspiciatis debitis ipsam neque minus reprehenderit doloribus,
+        natus dicta voluptas nemo beatae iusto quisquam nostrum, deserunt
+        commodi, explicabo laudantium?
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- my-test/src/index.js
+++ my-test/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import { AppProvider } from "./context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
-    <App />
- 
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
