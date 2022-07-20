4c131732b60d44848d1691bcca14c7c129e4ec42
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-20 23:14:15 +0500

    cocktails try and catch push

--- my-test/src/context.js
+++ my-test/src/context.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 
 import { useCallback } from "react";
 
-const url = "https://www.thecocktail.com/api/json/v1/1/search.php?s=";
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
@@ -14,12 +14,17 @@ const AppProvider = ({ children }) => {
     setLoading(true);
     try {
       const response = await fetch(`${url} ${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
     } catch (error) {
       console.log(error);
       setLoading(false);
     }
   });
 
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
   return <AppContext.Provider value={loading}>{children}</AppContext.Provider>;
 };
 
