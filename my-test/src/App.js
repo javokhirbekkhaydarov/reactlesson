2f4f0cdd282bcb7e369e15f2e4231496621246a0
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-27 16:43:29 +0500

    added new lesson25 and new components

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Lesson22Coctalils/Navbar";
-import Home from "./components/Lesson22Coctalils/pages/Home";
-import About from "./components/Lesson22Coctalils/pages/About";
-import Error from "./components/Lesson22Coctalils/pages/Error";
-import SingleCocktail from "./components/Lesson22Coctalils/pages/SingleCocktail";
-
+import Main from "./components/lesson25Pagination/Main";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="cocktail/:id" element={<SingleCocktail />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
