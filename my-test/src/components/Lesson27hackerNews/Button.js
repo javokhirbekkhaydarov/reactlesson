18e0cb122d5fd85c4867d133aa665ee2b1399cb1
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-04 22:04:32 +0500

    added prev and next btn

--- my-test/src/components/Lesson27hackerNews/Button.js
+++ my-test/src/components/Lesson27hackerNews/Button.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { useGlobalContext } from "./Context";
 const Button = () => {
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
   return (
-    <div>Button</div>
-  )
-}
+    <div className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage("dec")}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button disabled={isLoading} onClick={handlePage("inc")}>
+        {" "}
+        next
+      </button>
+    </div>
+  );
+};
 
-export default Button
+export default Button;
--- my-test/src/components/Lesson27hackerNews/SearchForm.js
+++ my-test/src/components/Lesson27hackerNews/SearchForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
+import { useGlobalContext } from "./Context";
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const { query, handleSearch } = useGlobalContext();
 
-export default SearchForm
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search hacker news</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchForm;
