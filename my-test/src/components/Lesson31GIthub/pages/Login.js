fdc6715bcb91ebfbdf46685287ca130516c597e8
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-11 21:16:37 +0500

    added  login js

--- my-test/src/components/Lesson31GIthub/pages/Login.js
+++ my-test/src/components/Lesson31GIthub/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+import loginImg from "../images/login-img.svg";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-
-export default Login
+    <Wrapper>
+      <div className="container">
+        <img src={loginImg} alt="github users" />
+        <h1>github users</h1>
+        <button className="btn">Login | Sign up</button>
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
+export default Login;
