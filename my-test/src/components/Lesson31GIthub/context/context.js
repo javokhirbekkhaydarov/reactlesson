f19f1badb0c136e54ee2a7f57d550ace8bb06114
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-16 15:01:45 +0500

    lesson32

--- my-test/src/components/Lesson31GIthub/context/context.js
+++ my-test/src/components/Lesson31GIthub/context/context.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, Children } from "react";
 import axios from "axios";
+import mockUser from "./mockData/MockUser";
+import mockRepos from "./mockData/MockRepos";
+import mockFollowers from "./mockData/mockFollowers";
 
 const rootUrl = "https://api.github.com";
 
@@ -7,10 +10,14 @@ const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value={isLoading}>
+    <GithubContext.Provider value={(githubUser, repos, followers, isLoading)}>
       {children}
-    </GithubContext.Provider>  
+    </GithubContext.Provider>
   );
 };
 export { GithubProvider, GithubContext };
